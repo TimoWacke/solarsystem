d4f382281c09447a239ac24b2463abcd7c8e1ada
mvondero <michavsa@gmail.com>
2023-01-24 11:29:06 +0100

    Lagrange L4&L5

--- Particles.py
+++ Particles.py
@@ -81,7 +81,22 @@ def particleList(name):
         moon = Particle("Moon", 1, {"x": 100, "y": 4, "z": 0}, {
                         "x": 1, "y": 3, "z": 0}, "black")
         return [star, planet, moon]
-
+    
+    if name == "Lagrangepoints":
+        star = Particle("Star", 1000, {"x": 0, "y": 0, "z": 0}, {
+                        "x": 0, "y": 0, "z": 0}, "orange")
+        planet = Particle("Planet", 50, {"x": 100, "y": 0, "z": 0}, {
+                        "x": 0, "y": 3, "z": 0}, "cadetblue")
+        l1 = Particle("L1", 1, {"x": 100, "y": 0, "z": 0}, {
+                        "x": 0, "y": 3, "z": 0}, "cadetblue")
+        l2 = Particle("L2", 1, {"x": 100, "y": 0, "z": 0}, {
+                        "x": 0, "y": 3, "z": 0}, "cadetblue")
+        l3 = Particle("L3", 1, {"x": 100, "y": 0, "z": 0}, {
+                        "x": 0, "y": 3, "z": 0}, "cadetblue")
+        l4 = Particle("L4", 1, {"x": 100*np.cos(np.PI/3), "y":-100*np.sin(np.PI/3) , "z": 0}, {
+                        "x": 3*np.sin(np.PI/3), "y": 3*np.cos(np.PI/3), "z": 0}, "cadetblue")
+        l5 = Particle("L5", 1, {"x": 100*np.cos(np.PI/3), "y":100*np.sin(np.PI/3) , "z": 0}, {
+                        "x": -3*np.sin(np.PI/3), "y": 3*np.cos(np.PI/3), "z": 0}, "cadetblue")
 
 def removeTotalImpulse(particleList):
     totalImpulse = np.array([float(0), float(0), float(0)])
