22361bdd854cd8713579b6d0c6f1cf5e63c78b24
Timo Wacke <wacketimo@gmail.com>
2023-02-10 23:51:34 +0100

    fix

--- Execute.py
+++ Execute.py
@@ -8,6 +8,7 @@ p.removeOffsetSpeed()
 s = Simulate(h=0.05, n=10000)
 a = Animate(p, s.verlet)
 
+a.create_video()
 a.pathPlot()
 a.energyPlot()
 a.phaseSpace(True) #True to show 3d, False to show 2d
